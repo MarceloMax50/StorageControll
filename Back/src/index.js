bf97908448a070684fe59899242e025e5b92ff61
MarceloMax50 <maxmelo93@gmail.com>
2022-05-11 23:43:39 -0300

    cors

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
+var cors = require('cors')
 const server = express();
 const port = 3000;
+
+server.use(cors());
 
 server.use(express.json());
 server.use('/storageControll', routes);
